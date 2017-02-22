e7213254f81fdcf2655874a23da0c682048d0d78
Galen Emery <Galen Emery>
2017-02-22 15:39:58 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chocolatey_server
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chocolatey_server
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
